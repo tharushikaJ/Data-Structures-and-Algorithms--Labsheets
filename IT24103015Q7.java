e91396407f38bc74ef809b0e103a5e2aa3081e86
Tharushika Jayawardhane <jayawardhanetd@gmail.com>
2026-07-31 08:52:46 +0530

    lab1

---
+++ IT24103015Q7.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class IT24103015Q7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+
+        int max;
+        if (a > b && a > c) {
+            max = a;
+        } else if (b > c) {
+            max = b;
+        } else {
+            max = c;
+        }
+
+        System.out.println("The maximum number is: " + max);
+
+
+    }
+
+}
